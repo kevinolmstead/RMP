84ca71e3f17a5d5db36cad9dba78198899599b9a
kevinolmstead <kevinolmstead@outlook.com>
2017-11-17 03:14:53 +0000

    yes

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -1,12 +1,76 @@
-angular.module('list', [])
-    .controller('MainCtrl', ['$scope', function($scope){
-      $scope.posts = [];
-
-      $scope.addPost = function() {
-        $scope.posts.push({
-          name: $scope.professorName,
-          text: $scope.postEntry
-        })
+$(document).ready(function(){
+  var everything;
+  $.getJSON('roast', function(data){
+    console.log(data);
+    everything = "<div class='indivRoasts'>";
+    for(var roast in data){
+      com = data[roast];
+      everything += "<p><span class='labels'>Name: </span>" + com.Professor + "<br><span class='labels'>Roast: </span>" + com.Roast + "</p></div>";
+    }
+    everything += "</div>";
+    $("#roasts").html(everything);
+  });
+  $("#roastButton").click(function(){
+    var myobj = {Professor:$("#professorField").val(),Roast:$("#roastText").val()};
+    //console.log($("#professorField").val());
+    //console.log($("#roastText").val());
+    jobj = JSON.stringify(myobj);
+    //$("#json").text(jobj);
+    var url = "roast";
+    $.ajax({
+      url:url,
+      type: "POST",
+      data: jobj,
+      contentType: "application/json; charset=utf-8",
+      success: function(data,textStatus) {
+      $.getJSON('roast', function(data){
+      console.log(data);
+      everything = "<div class='indivRoasts'>";
+      for(var roast in data) {
+        com = data[roast];
+        everything += "<p><span class='labels'>Name: </span>" + com.Professor + "<br><span class='labels'>Roast: </span>" + com.Roast + "</p></div>";
       }
-    }])
+      everything += "</div>";
+      $("#roasts").html(everything);
+    })
+
+      //console.log(myobj.Professor);
+      
+        //$("#done").html(textStatus);
+        /*var everything = "<div class='indivRoasts'>";
+        for(var roast in data) {
+          com = data[roast];
+          everything += "<p class='indiv'>Name: " + com.Professor + "<br>Roast: " + com.Roast + "</p></div>";
+          $("#roasts").html(everything);*/
+      }
+    })
+  });
+
+  /*$("#getRoasts").click(function(){
+    $.getJSON('roast', function(data){
+      console.log(data);
+      everything = "<div class='indivRoasts'>";
+      for(var roast in data) {
+        com = data[roast];
+        everything += "<p class='indiv'>Name: " + com.Professor + "<br>Roast: " + com.Roast + "</p></div>";
+      }
+      everything += "</div>";
+      $("#roasts").html(everything);
+    })
+  });*/
+
+  $("#deleteRoasts").click(function(){
+    var url = 'roast';
+    $.ajax({
+      url:url,
+      type: "DELETE",
+      success: function(data,textStatus){
+        //$("#done").html(textStatus);
+      }
+    })
+    everything = "";
+    $("#roasts").html(everything);
+  });
+});
+
 
--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,53 @@
 var express = require('express');
 var router = express.Router();
-var engines = require('consolidate');
-//router.engine('html', engines.mustache);
-//router.set('view engine', 'html');
+var mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/commentDB',{useMongoClient:true});
+var roastSchema = mongoose.Schema({
+  Professor:String,
+  Roast:String
+});
+
+var Roast = mongoose.model('Roast',roastSchema);
+
+var db = mongoose.connection;
+
+db.on('error', console.error.bind(console,'connection error:'));
+db.once('open',function(){ console.log("connected")});
 
 /* GET home page. */
-router.get('/', function(req, res) {
-  res.sendFile('roast.html', { root: 'public' });
-})
+router.post('/roast', function(req,res,next){
+  console.log("roast");
+  console.log(req.body);
+  var newcomment = new Roast(req.body); //[3]
+  console.log(newcomment); //[3]
+  newcomment.save(function(err, post) { //[4]
+    if (err) return console.error(err);
+    console.log(post);
+    res.sendStatus(200);
+  });
+});
+
+router.get('/roast', function(req, res, next) {
+  console.log("roast");
+  Roast.find(function(err,roastList){
+    if(err) return console.error(err);
+    else{
+      console.log(roastList);
+      res.json(roastList);
+    }
+  });
+});
+
+router.delete('/roast',function(req,res,next){
+  console.log("roast");
+  Roast.remove(function(err){
+    if(err) return console.error(err);
+    else{
+      console.log("delete worked");
+      res.sendStatus(200);
+    }
+  });
+});
+
 
 module.exports = router;
